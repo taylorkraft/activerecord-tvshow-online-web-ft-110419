fba6a5bc572212ad804da6480f09bde0b2a327d7
Taylor Kraft <taylorkraft16@gmail.com>
2020-01-02 17:21:19 +0000

    Automatically backed up by Learn

---
+++ app/models/show.rb
@@ -0,0 +1,29 @@
+class Show < ActiveRecord::Base
+  def highest_rating
+    Show.maximum(:rating)
+  end
+
+  def most_popular_show
+    Show.highest_rating
+  end
+
+  def lowest_rating
+    Show.minimum(:rating)
+  end
+
+  def least_popular_show
+    Show.lowest_rating
+  end
+
+  def ratings_sum
+    Show.sum(:rating)
+  end
+
+  def popular_shows
+    Show.where('rating > ?', 5)
+  end
+
+  def shows_by_alphabetical_order
+    Show.order
+  end
+end
